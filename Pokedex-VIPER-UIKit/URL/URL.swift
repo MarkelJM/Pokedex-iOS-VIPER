40d54d9eb3e6c671fb12c3832c13f607ee35c0c2
Markel <markjuaristi@gmail.com>
2023-06-05 09:33:10 +0200

    creo proyecto y añado la ulr json de la pokeapi

---
+++ Pokedex-VIPER-UIKit/URL/URL.swift
@@ -0,0 +1,13 @@
+//
+//  URL.swift
+//  Pokedex-VIPER-UIKit
+//
+//  Created by Markel Juaristi on 5/6/23.
+//
+
+import Foundation
+
+class ConstantsURL {
+    static var api_base_url = "https://pokedex-bb36f.firebaseio.com/pokemon.json"
+}
+
